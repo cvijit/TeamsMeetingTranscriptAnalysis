d7de9a4c2ca2ff28319c35b5c5aca98e8531d13e
Vijit Chekkala <55578649+cvijit@users.noreply.github.com>
2023-08-15 12:07:45 +0100

    Update TeamsTranscriptAnalysis.py

--- TeamsTranscriptAnalysis.py
+++ TeamsTranscriptAnalysis.py
@@ -5,72 +5,7 @@ import matplotlib.pyplot as plt
 from googletrans import Translator
 import spacy
 
-# Function to perform sentiment analysis
-def perform_sentiment_analysis(transcript):
-    blob = TextBlob(transcript)
-    sentiment_score = blob.sentiment.polarity
-    if sentiment_score < 0:
-        sentiment = "Negative"
-    elif sentiment_score == 0:
-        sentiment = "Neutral"
-    else:
-        sentiment = "Positive"
-    return sentiment, sentiment_score
-
-# Function for keyword extraction using TF-IDF
-def extract_keywords(transcript):
-    # Your keyword extraction code here using nltk or gensim
-    # For example, using TF-IDF from sklearn
-    from sklearn.feature_extraction.text import TfidfVectorizer
-    vectorizer = TfidfVectorizer()
-    tfidf_matrix = vectorizer.fit_transform([transcript])
-    keywords = vectorizer.get_feature_names_out()
-    return keywords
-
-# Function for named entity recognition using spaCy
-def named_entity_recognition(transcript):
-    # Your NER code here using spaCy
-    # For example, using pre-trained spaCy model for English
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp(transcript)
-    entities = [(ent.text, ent.label_) for ent in doc.ents]
-    return entities
-
-# Function for summary generation using TextRank algorithm
-def generate_summary(transcript):
-    # Your summary generation code here using gensim
-    # For example, using the TextRank algorithm
-    from gensim.summarization import summarize
-    summary = summarize(transcript)
-    return summary
-
-# Function for language translation using Googletrans
-def translate_to_languages(transcript, languages):
-    translator = Translator()
-    translated_transcripts = {}
-    for lang in languages:
-        translation = translator.translate(transcript, dest=lang)
-        translated_transcripts[lang] = translation.text
-    return translated_transcripts
-
-# Function for word cloud generation
-def generate_word_cloud(transcript):
-    wordcloud = WordCloud(width=800, height=400, background_color="white").generate(transcript)
-    plt.figure(figsize=(10, 5))
-    plt.imshow(wordcloud, interpolation="bilinear")
-    plt.axis("off")
-    plt.show()
-
-# Function for emotion analysis (basic sentiment analysis)
-def emotion_analysis(transcript):
-    emotions = perform_sentiment_analysis(transcript)
-    return emotions
-
-# Function for time-based analysis (custom implementation required)
-def time_based_analysis(transcript):
-    # Your time-based analysis code here using pandas and visualization libraries
-    # This function needs custom implementation based on specific requirements
-    return time_analysis_data
+# ... (function definitions)
 
 # Streamlit App
 def main():
@@ -79,36 +14,58 @@ def main():
 
     if uploaded_file is not None:
         transcript = uploaded_file.read().decode("utf-8")
-        st.subheader("Sentiment Analysis")
-        sentiment, sentiment_score = perform_sentiment_analysis(transcript)
-        st.write(f"Overall sentiment: {sentiment}, Sentiment score: {sentiment_score}")
 
-        st.subheader("Keyword Extraction")
-        keywords = extract_keywords(transcript)
-        st.write(keywords)
+        analysis_options = [
+            "Sentiment Analysis",
+            "Keyword Extraction",
+            "Named Entity Recognition (NER)",
+            "Summary Generation",
+            "Language Translation",
+            "Word Cloud Generation",
+            "Emotion Analysis",
+            "Time-Based Analysis"
+        ]
 
-        st.subheader("Named Entity Recognition (NER)")
-        entities = named_entity_recognition(transcript)
-        st.write(entities)
+        selected_analysis = st.selectbox("Select Analysis", analysis_options)
 
-        st.subheader("Summary Generation")
-        summary = generate_summary(transcript)
-        st.write(summary)
+        if selected_analysis == "Sentiment Analysis":
+            sentiment, sentiment_score = perform_sentiment_analysis(transcript)
+            st.subheader("Sentiment Analysis")
+            st.write(f"Overall sentiment: {sentiment}, Sentiment score: {sentiment_score}")
 
-        st.subheader("Language Translation")
-        languages = ["Spanish", "French", "German", "Chinese", "Japanese", "Arabic", "Russian", "Portuguese", "Italian", "Korean"]
-        translated_transcripts = translate_to_languages(transcript, languages)
-        st.write(translated_transcripts)
+        elif selected_analysis == "Keyword Extraction":
+            keywords = extract_keywords(transcript)
+            st.subheader("Keyword Extraction")
+            st.write(keywords)
 
-        st.subheader("Word Cloud Generation")
-        generate_word_cloud(transcript)
+        elif selected_analysis == "Named Entity Recognition (NER)":
+            entities = named_entity_recognition(transcript)
+            st.subheader("Named Entity Recognition (NER)")
+            st.write(entities)
 
-        st.subheader("Emotion Analysis")
-        emotions = emotion_analysis(transcript)
-        st.write(emotions)
+        elif selected_analysis == "Summary Generation":
+            summary = generate_summary(transcript)
+            st.subheader("Summary Generation")
+            st.write(summary)
 
-        st.subheader("Time-Based Analysis")
-        st.write("Custom implementation required for time-based analysis.")
+        elif selected_analysis == "Language Translation":
+            languages = ["Spanish", "French", "German", "Chinese", "Japanese", "Arabic", "Russian", "Portuguese", "Italian", "Korean"]
+            translated_transcripts = translate_to_languages(transcript, languages)
+            st.subheader("Language Translation")
+            st.write(translated_transcripts)
+
+        elif selected_analysis == "Word Cloud Generation":
+            st.subheader("Word Cloud Generation")
+            generate_word_cloud(transcript)
+
+        elif selected_analysis == "Emotion Analysis":
+            emotions = emotion_analysis(transcript)
+            st.subheader("Emotion Analysis")
+            st.write(emotions)
+
+        elif selected_analysis == "Time-Based Analysis":
+            st.subheader("Time-Based Analysis")
+            st.write("Custom implementation required for time-based analysis.")
 
 if __name__ == "__main__":
     main()
