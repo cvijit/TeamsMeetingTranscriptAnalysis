95cbcc54f97de788ebc65edefa39bdb57719a8a4
Vijit Chekkala <55578649+cvijit@users.noreply.github.com>
2023-09-11 15:58:58 +0100

    Update TeamsTranscriptAnalysis.py

--- TeamsTranscriptAnalysis.py
+++ TeamsTranscriptAnalysis.py
@@ -23,21 +23,58 @@ def main():
     sentiment, sentiment_score = perform_sentiment_analysis(transcript)
 
     st.subheader("Sentiment Analysis")
-    st.write(f"Overall sentiment: {sentiment}, Sentiment score: {sentiment_score}")
+    st.write(f"Sentiment: {sentiment}, Sentiment Score: {sentiment_score:.2f}")
+
+    # Highlight text based on sentiment
+    highlight_text(transcript, sentiment)
 
 # Function to perform sentiment analysis
 def perform_sentiment_analysis(transcript):
     blob = TextBlob(transcript)
     sentiment_score = blob.sentiment.polarity
 
-    if sentiment_score < 0:
+    if sentiment_score < -0.2:
+        sentiment = "Very Negative"
+    elif -0.2 <= sentiment_score < 0:
         sentiment = "Negative"
     elif sentiment_score == 0:
         sentiment = "Neutral"
-    else:
+    elif 0 < sentiment_score <= 0.2:
         sentiment = "Positive"
+    else:
+        sentiment = "Very Positive"
 
     return sentiment, sentiment_score
+
+# Function to highlight text based on sentiment
+def highlight_text(text, sentiment):
+    sentiment_colors = {
+        "Very Negative": "red",
+        "Negative": "orange",
+        "Neutral": "gray",
+        "Positive": "green",
+        "Very Positive": "lime",
+    }
+
+    # Highlight text based on sentiment
+    st.markdown(f"**Highlighted Text ({sentiment}):**")
+    for sentence in TextBlob(text).sentences:
+        sentence_sentiment = sentence.sentiment.polarity
+        color = sentiment_colors.get(get_sentiment_label(sentence_sentiment), "gray")
+        st.markdown(f"<span style='background-color:{color}'>{sentence}</span>", unsafe_allow_html=True)
+
+# Helper function to get sentiment label
+def get_sentiment_label(sentiment_score):
+    if sentiment_score < -0.2:
+        return "Very Negative"
+    elif -0.2 <= sentiment_score < 0:
+        return "Negative"
+    elif sentiment_score == 0:
+        return "Neutral"
+    elif 0 < sentiment_score <= 0.2:
+        return "Positive"
+    else:
+        return "Very Positive"
 
 if __name__ == "__main__":
     main()
