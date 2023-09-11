bfca334b3f64238977ff3ae73d2d96fe670aa6a0
Vijit Chekkala <55578649+cvijit@users.noreply.github.com>
2023-09-11 15:44:42 +0100

    Update TeamsTranscriptAnalysis.py

--- TeamsTranscriptAnalysis.py
+++ TeamsTranscriptAnalysis.py
@@ -1,71 +1,43 @@
 import streamlit as st
 from textblob import TextBlob
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-from googletrans import Translator
-import spacy
-
-# ... (function definitions)
 
 # Streamlit App
 def main():
-    st.title("Teams Meeting Transcript Analysis")
-    uploaded_file = st.file_uploader("Upload Teams meeting transcript (txt file)", type=["txt"])
+    st.title("Sentiment Analysis App")
+    
+    # Add a textarea for manual text input
+    text_input = st.text_area("Enter text for sentiment analysis:")
+    
+    uploaded_file = st.file_uploader("Upload a text file (txt)", type=["txt"])
 
-    if uploaded_file is not None:
+    if text_input:
+        transcript = text_input
+
+    elif uploaded_file is not None:
         transcript = uploaded_file.read().decode("utf-8")
+    else:
+        st.warning("Please provide text or upload a file for analysis.")
+        return
 
-        analysis_options = [
-            "Sentiment Analysis",
-            "Keyword Extraction",
-            "Named Entity Recognition (NER)",
-            "Summary Generation",
-            "Language Translation",
-            "Word Cloud Generation",
-            "Emotion Analysis",
-            "Time-Based Analysis"
-        ]
+    # Perform sentiment analysis
+    sentiment, sentiment_score = perform_sentiment_analysis(transcript)
 
-        selected_analysis = st.selectbox("Select Analysis", analysis_options)
+    st.subheader("Sentiment Analysis")
+    st.write(f"Overall sentiment: {sentiment}, Sentiment score: {sentiment_score}")
 
-        if selected_analysis == "Sentiment Analysis":
-            sentiment, sentiment_score = perform_sentiment_analysis(transcript)
-            st.subheader("Sentiment Analysis")
-            st.write(f"Overall sentiment: {sentiment}, Sentiment score: {sentiment_score}")
+# Function to perform sentiment analysis
+def perform_sentiment_analysis(transcript):
+    blob = TextBlob(transcript)
+    sentiment_score = blob.sentiment.polarity
 
-        elif selected_analysis == "Keyword Extraction":
-            keywords = extract_keywords(transcript)
-            st.subheader("Keyword Extraction")
-            st.write(keywords)
+    if sentiment_score < 0:
+        sentiment = "Negative"
+    elif sentiment_score == 0:
+        sentiment = "Neutral"
+    else:
+        sentiment = "Positive"
 
-        elif selected_analysis == "Named Entity Recognition (NER)":
-            entities = named_entity_recognition(transcript)
-            st.subheader("Named Entity Recognition (NER)")
-            st.write(entities)
-
-        elif selected_analysis == "Summary Generation":
-            summary = generate_summary(transcript)
-            st.subheader("Summary Generation")
-            st.write(summary)
-
-        elif selected_analysis == "Language Translation":
-            languages = ["Spanish", "French", "German", "Chinese", "Japanese", "Arabic", "Russian", "Portuguese", "Italian", "Korean"]
-            translated_transcripts = translate_to_languages(transcript, languages)
-            st.subheader("Language Translation")
-            st.write(translated_transcripts)
-
-        elif selected_analysis == "Word Cloud Generation":
-            st.subheader("Word Cloud Generation")
-            generate_word_cloud(transcript)
-
-        elif selected_analysis == "Emotion Analysis":
-            emotions = emotion_analysis(transcript)
-            st.subheader("Emotion Analysis")
-            st.write(emotions)
-
-        elif selected_analysis == "Time-Based Analysis":
-            st.subheader("Time-Based Analysis")
-            st.write("Custom implementation required for time-based analysis.")
+    return sentiment, sentiment_score
 
 if __name__ == "__main__":
     main()
