258a0ec7bb68af64f0d8b7ac4d5c0afa2038bb45
Vijit Chekkala <55578649+cvijit@users.noreply.github.com>
2023-09-12 10:19:53 +0100

    Update TeamsTranscriptAnalysis.py

--- TeamsTranscriptAnalysis.py
+++ TeamsTranscriptAnalysis.py
@@ -1,28 +1,50 @@
 import streamlit as st
 from textblob import TextBlob
 import nltk
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
+import tweepy
 
 # Download NLTK corpora for sentence tokenization
 nltk.download("punkt")
+
+# Twitter API Authentication (No user login required)
+def authenticate_twitter_api():
+    consumer_key = "your_consumer_key"
+    consumer_secret = "your_consumer_secret"
+    access_token = "your_access_token"
+    access_token_secret = "your_access_token_secret"
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    
+    api = tweepy.API(auth)
+    return api
 
 # Streamlit App
 def main():
     st.title("Sentiment Analysis App")
     
-    # Add a textarea for manual text input
-    text_input = st.text_area("Enter text for sentiment analysis:")
+    # Add a selectbox to choose input type
+    input_type = st.selectbox("Select input type:", ["Text Input", "Upload File", "Twitter Hashtags"])
     
-    uploaded_file = st.file_uploader("Upload a text file (txt)", type=["txt"])
-
-    if text_input:
+    if input_type == "Text Input":
+        text_input = st.text_area("Enter text for sentiment analysis:")
         transcript = text_input
-
-    elif uploaded_file is not None:
-        transcript = uploaded_file.read().decode("utf-8")
+    elif input_type == "Upload File":
+        uploaded_file = st.file_uploader("Upload a text file (txt)", type=["txt"])
+        if uploaded_file is not None:
+            transcript = uploaded_file.read().decode("utf-8")
+    elif input_type == "Twitter Hashtags":
+        hashtag_input = st.text_input("Enter hashtags (comma-separated):")
+        if hashtag_input:
+            hashtags = [tag.strip() for tag in hashtag_input.split(',')]
+            tweets = fetch_tweets_by_hashtags(hashtags)
+            if tweets:
+                transcript = "\n".join([tweet.text for tweet in tweets])
+            else:
+                st.warning("No tweets found for the specified hashtags.")
+                return
     else:
-        st.warning("Please provide text or upload a file for analysis.")
+        st.warning("Please select an input type.")
         return
 
     # Perform sentiment analysis
@@ -88,13 +110,22 @@ def get_sentiment_label(sentiment_score):
 # Function to display the legend for highlighted colors
 def display_legend():
     st.markdown("### Legend for Highlighted Colors:")
-    fig, ax = plt.subplots(figsize=(6, 1))
-    colors = ["red", "orange", "gray", "green", "lime"]
-    labels = ["Very Negative", "Negative", "Neutral", "Positive", "Very Positive"]
-    patches = [mpatches.Patch(color=colors[i], label=labels[i]) for i in range(len(colors))]
-    ax.legend(handles=patches, loc='center', frameon=False)
-    ax.axis('off')
-    st.pyplot(fig)
+    st.markdown("- <span style='background-color:red'>Red</span>: Very Negative")
+    st.markdown("- <span style='background-color:orange'>Orange</span>: Negative")
+    st.markdown("- <span style='background-color:gray'>Gray</span>: Neutral")
+    st.markdown("- <span style='background-color:green'>Green</span>: Positive")
+    st.markdown("- <span style='background-color:lime'>Lime</span>: Very Positive")
+
+# Define a function to fetch tweets based on hashtags using the Twitter API
+def fetch_tweets_by_hashtags(hashtags):
+    api = authenticate_twitter_api()
+    
+    tweets = []
+    for hashtag in hashtags:
+        hashtag_tweets = api.search(q=f"#{hashtag}", count=10)  # Adjust count as needed
+        tweets.extend(hashtag_tweets)
+    
+    return tweets
 
 if __name__ == "__main__":
     main()
